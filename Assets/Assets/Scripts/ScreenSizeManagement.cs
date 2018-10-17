f015ee71307703301a3032679f90670ffeb58ab0
Ben Tarrant <ben.g.tarrant@gmail.com>
2018-10-17 17:16:50 +0100

    UI
    FIXED issue where screen resolution wouldn't change based on screen
    
    FIXED issue where UI wasn't achored correctly

---
+++ Assets/Assets/Scripts/ScreenSizeManagement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSizeManagement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
